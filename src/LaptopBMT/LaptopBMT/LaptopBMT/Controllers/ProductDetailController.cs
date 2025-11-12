53ff5e50cf61d9f7acafdd47084fdfc6788a05b0
hoavv <hoavv2602913@tvu-onschool.edu.vn>
2025-11-12 17:12:39 +0700

    up project
    up project

---
+++ src/LaptopBMT/LaptopBMT/LaptopBMT/Controllers/ProductDetailController.cs
@@ -0,0 +1,31 @@
+﻿using LaptopBMT.Data;
+using Microsoft.AspNetCore.Mvc;
+
+public class ProductDetailController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ProductDetailController(AppDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Detail(int id)
+    {
+        var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+        if (product == null)
+            return NotFound(); // hoặc RedirectToAction("Index");
+
+        return View(product);
+    }
+/*
+    public IActionResult Detail(int id)
+    {
+        var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    } */
+}
---
+++ src/LaptopBMT/LaptopBMT/LaptopBMT/Models/Cart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaptopBMT.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }   // Khóa ngoại
+
+        public User? User { get; set; }    // Điều hướng tới entity User
+
+        public string? UserName { get; set; }
+        // ✅ Đánh dấu giỏ hàng đã thanh toán hay chưa
+        public bool IsCheckedOut { get; set; } = false;
+        public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
